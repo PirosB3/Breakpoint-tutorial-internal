16b65713dc2b606b352dafa0660b6e0b6bf57f7f
Daniel Pyrathon <daniel@phantom.app>
2022-11-04 15:12:21 +0000

    Remove watch

--- watch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export ANCHOR_PROVIDER_URL=http://localhost:8899
-export ANCHOR_WALLET='/Users/danielpyrathon/.config/solana/id.json'
-npx ts-mocha -p ./tsconfig.json -t 1000000 'tests/*.test.ts' --watch-extensions ts --watch --watch-files 'tests/*.ts'
